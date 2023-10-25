fa5c455a53d17091e48d8b6b44a3dcb1bd729f0c
AlexisCepeda <alexisnatel@gmail.com>
2023-10-25 03:13:14 -0300

    update basic queries

--- src/main/java/desarrollo/sprint4/apiresttest/Repository/ArticuloInsumoRepository.java
+++ src/main/java/desarrollo/sprint4/apiresttest/Repository/ArticuloInsumoRepository.java
@@ -1,8 +1,28 @@
 package desarrollo.sprint4.apiresttest.Repository;
 
 import desarrollo.sprint4.apiresttest.Entity.ArticuloInsumo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticuloInsumoRepository extends BaseRepository<ArticuloInsumo,Long>{
+
+    @Query(
+            value = "SELECT * FROM articulo_insumo WHERE articulo_insumo.nombre_articulo_insumo LIKE %:nombre%",
+            nativeQuery = true
+    )
+    Page<ArticuloInsumo> searchByNombre(
+            @Param("nombre") String nombre,
+            Pageable pageable
+    );
+
+    @Query(value = "SELECT ai FROM ArticuloInsumo ai JOIN ai.rubro r WHERE r.nombreRubro LIKE %:nombreRubro%")
+    Page<ArticuloInsumo> searchByRubroNombre(
+            @Param("nombreRubro") String nombreRubro,
+            Pageable pageable
+    );
+
 }
