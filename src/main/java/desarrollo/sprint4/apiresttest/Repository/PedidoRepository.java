2c2e58dca705dc90590a8ddb413070dae89ee436
AlexisCepeda <alexisnatel@gmail.com>
2023-10-24 23:21:35 -0300

    added Query searchHistorialPedidoByCliente

--- src/main/java/desarrollo/sprint4/apiresttest/Repository/PedidoRepository.java
+++ src/main/java/desarrollo/sprint4/apiresttest/Repository/PedidoRepository.java
@@ -1,8 +1,22 @@
 package desarrollo.sprint4.apiresttest.Repository;
 
+import desarrollo.sprint4.apiresttest.Entity.ArticuloManufacturado;
 import desarrollo.sprint4.apiresttest.Entity.Pedido;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PedidoRepository extends BaseRepository<Pedido, Long> {
+
+
+    //Para buscar el hitorial de pedidos relaizados por un cliente
+    @Query("SELECT p FROM Pedido p JOIN p.cliente c WHERE c.id = :idCliente ORDER BY p.fechaPedido DESC")
+    Page<Pedido> searchHistorialPedidoByCliente(
+            @Param("idCliente") Long idCliente,
+            Pageable pageable
+    );
+
 }
